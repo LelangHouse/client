a9cf15bb7ad0775a2a619473fda1fb8b4d5fe758
Amil Hasbala <amilhasbala@gmail.com>
2019-12-05 22:13:38 +0700

    login register

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <template>
-  
+  <div>
+    <LoginRegisterpage @set-login="setLogin"></LoginRegisterpage>
+  </div>
 </template>
 
 <script>
+import LoginRegisterpage from "./views/LoginRegisterpage";
 export default {
-
-}
+  components: {
+    LoginRegisterpage
+  },
+  methods: {
+    setLogin() {
+      console.log("login sampai app.vue");
+    }
+  }
+};
 </script>
 
 <style>
-
 </style>
---
+++ src/components/RegisterForm.vue
@@ -0,0 +1,67 @@
+<template>
+  <div
+    class="tab-pane fade show active"
+    id="pills-register"
+    role="tabpanel"
+    aria-labelledby="pills-register-tab"
+  >
+    <form>
+      <div class="form-group">
+        <input
+          type="text"
+          name="username"
+          id="name"
+          class="form-control"
+          placeholder="Username"
+          required
+          autofocus
+        />
+      </div>
+
+      <div class="form-group">
+        <input
+          type="email"
+          name="email"
+          id="email"
+          class="form-control"
+          placeholder="Email"
+          required
+        />
+      </div>
+      <div class="form-group">
+        <input
+          type="text"
+          name="phone"
+          id="phone"
+          class="form-control"
+          placeholder="Phone"
+          required
+        />
+      </div>
+
+      <div class="form-group">
+        <input
+          type="password"
+          name="password"
+          id="password"
+          class="form-control"
+          placeholder="Set a password"
+          required
+        />
+      </div>
+
+      <div class="text-center pt-2 pb-1">
+        <button type="submit" class="btn btn-primary">Register</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RegisterForm"
+};
+</script>
+
+<style>
+</style>
