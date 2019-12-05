c234ca11822f32fdfdf730498a6d378a60c5abcc
sukmabrahmantya <brahmantyasukma@gmail.com>
2019-12-05 19:26:44 +0700

    first commit to mainPage hohoho

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
-  
+  <div>
+    <MainPage></MainPage>
+  </div>
 </template>
 
 <script>
+import MainPage from "./views/MainPage";
 export default {
-
-}
+  components: {
+    MainPage
+  }
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
---
+++ src/views/MainPage.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div class="container mt-5">
+      <div class="card-columns">
+        <div class="card mt-3">
+          <img
+            class="card-img-top"
+            alt="Card image cap"
+            src="https://images.unsplash.com/photo-1532910404247-7ee9488d7292?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2682&q=80"
+            width="100px"
+          />
+          <div class="card-body">
+            <h3 class="card-title">NAMA BARANG</h3>
+            <div class="row mb-2">
+              <div class="col-sm-4">Offered Price</div>
+              <div class="col-sm-8">RP.0001010</div>
+            </div>
+            <div class="row mb-2">
+              <div class="col-sm-4">Highest Bid</div>
+              <div class="col-sm-8">Rp.10201</div>
+            </div>
+          </div>
+          <a href="#" style="text-decoration: none"
+            ><button type="button" class="btn btn-info btn-lg btn-block">
+              Submit a Bid
+            </button></a
+          >
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MainPage"
+};
+</script>
+
+<style></style>
