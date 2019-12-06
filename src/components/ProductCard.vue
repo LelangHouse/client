784e377b5e12426014d0172e98cd0ca61334f5dc
sukmabrahmantya <brahmantyasukma@gmail.com>
2019-12-06 13:10:50 +0700

    adding somethink

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="card mt-3">
+    <div class="card mt-3" style="border-radius: 5px">
       <img
         class="card-img-top"
         alt="Card image cap"
@@ -9,19 +9,41 @@
       />
       <div class="card-body">
         <h3 class="card-title">{{ databapak.bid }} IDR</h3>
-        <div class="row mb-2">
+        <div class="row">
           <div class="col-sm-6">Offered Price</div>
           <div class="col-sm-6">{{ databapak.price }} IDR</div>
         </div>
       </div>
-      <b-button
-        @click="UpdateBidModal"
-        type="button"
-        class="btn btn-info btn-lg btn-block"
-      >
-        Bid
-      </b-button>
-      <div></div>
+
+      <div class="container">
+        <b-button
+          @click="UpdateBidModal"
+          type="button"
+          class="btn btn-info btn-lg btn-block"
+        >
+          Bid
+        </b-button>
+      </div>
+      <div class="container mt-2 mb-2">
+        <b-row>
+          <b-col
+            ><b-button
+              @click="UpdateBidModal"
+              type="button"
+              class="btn btn-info btn-lg btn-block"
+            >
+              W</b-col>
+          <b-col
+            ><b-button
+              @click="UpdateBidModal"
+              type="button"
+              class="btn btn-info btn-lg btn-block"
+            >
+              T
+            </b-button></b-col
+          >
+        </b-row>
+      </div>
     </div>
   </div>
 </template>
@@ -37,5 +59,8 @@ export default {
   }
 };
 </script>
-
-<style></style>
+<style scoped>
+  button {
+    border-radius: 0px;
+  }
+</style>
