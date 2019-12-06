3a2a19e650fac57adf638d8da1b6dfc433986f89
hoandreas <andreas_ho@hotmail.com>
2019-12-06 13:09:02 +0700

    merged form and mainpage

--- src/components/formUpload.vue
+++ src/components/formUpload.vue
@@ -1,107 +1,93 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center mt-3">
-            <div class="col-10">
-              <div class="box border p-3 shadow text-center">
-                  <p style="font-family: 'Righteous', cursive;">Upload your house</p>
-                  <hr>
-                <!-- <form>
-                    <div class="row">
-                      <div class="col-12">
-                        <div class="form-group">
-                            <label for="file" class="sr-only">File</label>
-                            <div class="input-group">
-                                <input v-model='image' type="text" name="filename" class="form-control" placeholder="No file selected" readonly>
-                                <span class="input-group-btn">
-                                <div class="btn btn-default  custom-file-uploader py-0">
-                                    <input  type="file" name="file" onchange="this.form.filename.value = this.files.length ? this.files[0].name : ''" />
-                                <span class="btn btn-primary"> Select a file </span>
-                                </div>
-                                </span>
-                            </div>
-                        </div>
-                        <div class="input-group mb-3">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text">Rp</span>
-                        </div>
-                        <input v-model='price' type="text" class="form-control" aria-label="Amount (to the nearest dollar)">
-                        <div class="input-group-append">
-                            <span class="input-group-text">.00</span>
-                        </div>
-                        </div>
-                      </div>
-                    </div>
-                   
-                </form> -->
-                <b-row>
-        <b-col>
-                <b-form-file
+  <div class="container">
+    <div class="row justify-content-center mt-3">
+      <div class="col-10">
+        <div class="box border p-3 shadow text-center">
+          <p style="font-family: 'Righteous', cursive;">Upload your house</p>
+          <hr />
+          <b-row>
+            <b-col>
+              <label for="input-default">Price:</label>
+              <b-form-input
+                v-model="price"
+                id="input-default"
+                placeholder="Enter your house price"
+              ></b-form-input>
+            </b-col>
+          </b-row>
+
+          <b-row>
+            <b-col>
+              <b-form-file
                 v-model="image"
                 :state="Boolean(image)"
                 placeholder="Choose a file or drop it here..."
                 drop-placeholder="Drop file here..."
-                ></b-form-file>
-        </b-col>
-    </b-row>
- <button @click="addHouse" style="--content: 'Submit Your House';">
-                        <div class="left"></div>
-                            Submit Your House
-                        <div class="right"></div>
-                    </button>
-               </div> 
-            </div>
+              ></b-form-file>
+            </b-col>
+          </b-row>
+          <button @click="addHouse" style="--content: 'Submit Your House'; margin-top: 20px;">
+            <div class="left"></div>Submit Your House
+            <div class="right"></div>
+          </button>
         </div>
+      </div>
     </div>
-
- 
+  </div>
 </template>
 
 <script>
-import axios from 'axios'
-import Swal from "sweetalert2"
-import FormData2 from "form-data"
+import axios from "axios";
+import Swal from "sweetalert2";
 
 export default {
-    name: 'formUpload',
-    data() {
-        return{
-            price: null,
-            image: null,   
-        }
-    },
-    methods:{
-        addHouse() {
-            console.log('masuk')
-            const fromData = new FormData2()
-            fromData.append('image', this.image)
-            formData.set('price',this.price)
+  name: "formUpload",
+  data() {
+    return {
+      price: null,
+      image: null
+    };
+  },
+  methods: {
+    addHouse() {
+      const formData = new FormData();
+      formData.append("image", this.image);
+      formData.set("price", this.price);
 
-            axios({
-                method:"post",
-                url: "http://localhost:3000/products",
-                data: formData,
-                headers:{
-                    token:localStorage.getItem('token')
-                }
-            })
-            .then(({data})=>{
-              Swal.fire({
-                        icon: 'success',
-                        title: `Success posted a new article`,
-                        showConfirmButton: false,
-                        timer: 1500
-                    })
-            })
+      axios({
+        method: "post",
+        url: "http://localhost:3000/products",
+        data: formData,
+        headers: {
+          token: localStorage.getItem("token")
         }
+      })
+        .then(({ data }) => {
+          Swal.fire({
+            icon: "success",
+            title: `Success posted a house`,
+            showConfirmButton: false,
+            timer: 1500
+          })
+        })
+        .catch(err => {
+          console.log(err)
+          Swal.fire({
+              icon: 'error',
+              title: 'Oops...',
+              text: 'Internal server error'
+          })
+        })
     }
-}
+  }
+};
 </script>
 
 <style scoped>
 .custom-file-uploader {
   position: relative;
 }
-.custom-file-uploader input[type='file'] {
+.custom-file-uploader input[type="file"] {
   display: block;
   position: absolute;
   top: 0;
@@ -114,7 +100,6 @@ export default {
   opacity: 0;
   cursor: default;
 }
-
 
 * {
   box-sizing: border-box;
@@ -252,5 +237,4 @@ button::before {
     transform: translate(0, -66.666%);
   }
 }
-
 </style>
